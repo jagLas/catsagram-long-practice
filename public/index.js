b106d54cbb8e60c13a281a42b681f8227dd65212
jagLas <jglasher@gmail.com>
2023-03-17 13:45:02 -0400

    added function to fetch a new cat

--- public/index.js
+++ public/index.js
@@ -1,1 +1,11 @@
 // Your code here
+async function getCat() {
+     let cat = await fetch('https://api.thecatapi.com/v1/images/search')
+    cat = await cat.json();
+    return cat;
+}
+
+window.onload = () => {
+    console.log('scripts loading')
+    // getCat();
+}
