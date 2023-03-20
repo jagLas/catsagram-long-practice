e5a60966002cfee9715eb00077ad3799be91c364
jagLas <jglasher@gmail.com>
2023-03-20 10:16:43 -0400

    implemented upvote and downvote buttons

--- public/main.js
+++ public/main.js
@@ -13,7 +13,39 @@ export const createMainContent = () => {
     container.appendChild(img);
 
     fetchImage();
+
+    const popDiv = document.createElement('div');
+    popDiv.innerText = 'Popularity score: '
+    const score = document.createElement('span');
+    score.innerText = 0;
+    score.setAttribute('id', 'score');
+    popDiv.appendChild(score);
+    document.body.appendChild(popDiv);
+
+    const voteDiv = document.createElement('div');
+    const upVote = document.createElement('button');
+    upVote.innerText = 'Upvote';
+    upVote.setAttribute('id', 'up-vote');
+    voteDiv.appendChild(upVote);
+    upVote.addEventListener('click', addVote);
+
+    const downVote = document.createElement('button');
+    downVote.setAttribute('id', 'down-vote');
+    downVote.innerText = 'Downvote';
+    voteDiv.appendChild(downVote);
+    document.body.appendChild(voteDiv);
+    downVote.addEventListener('click', removeVote);
 };
+
+function addVote (e) {
+    const score = document.querySelector('#score');
+    score.innerText = Number(score.innerText) + 1;
+}
+
+function removeVote(e) {
+    const score = document.querySelector('#score');
+    score.innerText = Number(score.innerText) - 1;
+}
 
 const fetchImage = async () => {
     // Fetch image from API and set img url
